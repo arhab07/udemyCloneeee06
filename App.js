796cd9b929eec7b935ef7961fb5d56c02492dc42
arhab07 <arhab07ah@gmail.com>
2023-06-06 10:41:21 +0530

    update3

--- App.js
+++ App.js
@@ -105,7 +105,7 @@ export default function App() {
         const update = await Updates.checkForUpdateAsync({ manifestUrl: 'https://raw.githubusercontent.com/arhab07/udemyCloneeee06/main/android-index.json' });
         if (update.isAvailable) {
           await Updates.fetchUpdateAsync();
-          // You can display a notification or prompt the user to reload here
+
           Updates.reloadAsync();
         }
       } catch (error) {
